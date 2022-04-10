6711c1261101104a698c3763521d127ea2830a4f
unknown <drishyakj22@gmail.com>
2022-04-10 10:07:25 +0530

    AddedDB

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ =require('lodash');
+const mongoose = require('mongoose');
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,8 +20,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/journalDB");
+const postSchema = new mongoose.Schema({
+  postTitle:String,
+  postBody:String
+});
+const Post= mongoose.model("Post",postSchema);
+
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContentKey:homeStartingContent,posts:journal});
+  Post.find(function(err,journal){
+    if(!err)
+    {
+      res.render("home",{homeStartingContentKey:homeStartingContent,posts:journal});
+    }
+  });
 
 });
 
@@ -32,38 +46,44 @@ app.get("/contact",function(req,res){
 });
 
 app.get("/compose",function(req,res){
-  res.render("compose");
+  res.render("compose");//call compose.ejs
 });
 
 app.post("/compose",function(req,res){
 
-  const newPost = {postTitle:req.body.titleText, postBody:req.body.bodyText};
-  journal.push(newPost);
-  res.redirect("/")
+  const newPost = new Post({postTitle:req.body.titleText, postBody:req.body.bodyText});
+  // journal.push(newPost);
+  newPost.save(function(err){
+    if(!err)
+    {
+      res.redirect("/")
+    }
+  });
+
 
 });
 
 app.get("/posts/:postId",function(req,res){ //Route parameter//same layout different content
-  requestedId = _.lowerCase(req.params.postId);
-
-  journal.forEach(function(post){
-    storedTitle =_.lowerCase(post.postTitle);
-    if( storedTitle=== requestedId)
-    {
-      res.render("post",{post:post});
-    }
+  // requestedId = _.lowerCase(req.params.postId);
+  requestedId = req.params.postId;
+  Post.findOne({_id:requestedId},function(err,post){
+    res.render("post",{post:post});
   });
+  // Post.find(function(err,journal){//do with _id in real world things. Dont use title
+  //   if(!err)
+  //   {
+  //     journal.forEach(function(post){
+  //       storedTitle =_.lowerCase(post.postTitle);
+  //       if( storedTitle=== requestedId)
+  //       {
+  //         res.render("post",{post:post});
+  //       }
+  //     });
+  //   }
+  // });
+
 
 });
-
-
-
-
-
-
-
-
-
 
 
 app.listen(3000, function() {
